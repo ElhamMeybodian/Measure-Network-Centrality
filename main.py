f3e1bba8257380d8450688ae4e2e9c7716cfb068
elhammey <elhammey@gmail.com>
2022-01-18 18:04:41 +0330

    update file

--- main.py
+++ main.py
@@ -9,33 +9,26 @@ plt.show()
 
 # degree centrality
 deg_centrality = nx.degree_centrality(g)
-# print(tabulate([(node,deg_centrality[node]) for node in deg_centrality], headers=['Node', 'Degree Centrality']))
-# print('\n')
+print(f'max degree centrality node {max(deg_centrality)} = {deg_centrality[max(deg_centrality)]}')
+
 
 # Closeness Centrality
 close_centrality = nx.closeness_centrality(g)
-# print(tabulate([(node,close_centrality[node]) for node in close_centrality], headers=['Node', 'Closeness Centrality']))
-# print('\n')
 
 # Betweenness Centrality
 bet_centrality = nx.betweenness_centrality(g, normalized=True,endpoints=False)
-# print(tabulate([(node,bet_centrality[node]) for node in bet_centrality], headers=['Node', 'Betweenness Centrality']))
-# print('\n')
 
 # Page Rank
+# The value of alpha is usually set between 0.8 to 0.9.
 pr = nx.pagerank(g, alpha = 0.8)
-# print(tabulate([(node,pr[node]) for node in pr], headers=['Node', 'Page Rank']))
-# print('\n')
+
 
 # Katz Centrality
 k_centrality = nx.katz_centrality(g)
-# print(tabulate([(node,k_centrality[node]) for node in k_centrality], headers=['Node', 'Katz Centrality']))
-# print('\n')
 
 
 # Eigenvector Centrality
 e_centrality = nx.eigenvector_centrality(g)
-# print(tabulate([(node,e_centrality[node]) for node in e_centrality], headers=['Node', 'Eigenvector Centrality']))
-# print('\n')
+
 
 print(tabulate([(node,deg_centrality[node],close_centrality[node],bet_centrality[node],pr[node],k_centrality[node],e_centrality[node]) for node in e_centrality], headers=['Node','Degree Centrality', 'Closeness Centrality', 'Betweenness Centrality','Page Rank', 'Katz Centrality','Eigenvector Centrality']))
